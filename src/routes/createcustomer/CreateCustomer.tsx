c1d3a600150800e0d8eed9efe6c6db799f558df8
RobertNevarez <lilgosu999@gmail.com>
2022-06-21 19:13:23 -0400

    Created customer can add customer

--- src/routes/createcustomer/CreateCustomer.tsx
+++ src/routes/createcustomer/CreateCustomer.tsx
@@ -1,124 +1,104 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './CreateCustomer.css'
 
 
 function CreateCustomer() {
+
+    let CusName: string = '';
+    let CusPhone: string = '';
+    let CusAddress: string = '';
+    let CusEmail : string = '';
+    let CusID : number = 0;
+    let ConfirmCusID : number = 0;
+    
+    function insertName(e : any)
+    {
+        CusName = e.target.value;
+        
+    }
+    function insertPhone(e : any)
+    {
+        CusPhone = e.target.value;
+    }
+    function insertAddress(e : any)
+    {
+        CusAddress = e.target.value;
+    }
+    function insertEmail(e : any)
+    {
+        CusEmail = e.target.value;
+    }
+    function insertCustID(e : any)
+    {
+        CusID = e.target.value;
+    }
+    function insertConfirmID(e : any)
+    {
+        ConfirmCusID = e.target.value;
+    }
+    
+    function onSubmit(e : any)
+    {
+        e.preventDefault();
+
+      fetch("http://codebebopp2project-env.eba-ag3aw5vp.us-east-1.elasticbeanstalk.com/api/Customer/AddCustomer", {
+      method: 'POST',
+       headers: {
+         'Accept': 'application/json',
+         'Content-Type': 'application/json'
+       },
+       body: JSON.stringify(
+           {
+               custID: CusID, 
+               name: CusName,
+               phone: CusPhone,
+               address: CusAddress,
+               email:CusEmail,
+               cAccID: ConfirmCusID
+           }
+           )
+     });
+
+  }
+
+
+
     return (
-        <form className='createcustomer-container'>
+        <form className='createcustomer-container' onSubmit={onSubmit}>
             <div className="form-group col-md-4 ">
                 <label htmlFor="inputName">Name</label>
                 <div>
-                <input type="text" className="form-control" id="inputAddress" placeholder="John Doe" />
+                <input type="text" className="form-control" id="inputAddress" placeholder="John Doe" onChange={insertName} />
                 </div>
-                <label htmlFor="inputEmail">Email</label>
+            </div>
+            <div className="form-group col-md-4 ">
+                <label htmlFor="inputPhoneNumber">Phone Number</label>
                 <div>
-                <input type="text" className="form-control" id="inputEmail" placeholder="johndoe@ymail.com" />
+                <input type="text" className="form-control" id="inputAddress" placeholder="1234567890" onChange={insertPhone} />
                 </div>
             </div>
             <div className="form-group col-md-4">
                 <label htmlFor="inputAddress">Address</label>
-                <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St" />
+                <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St" onChange={insertAddress} />
+                <label htmlFor="inputEmail">Email</label>
+                <div>
+                <input type="text" className="form-control" id="inputEmail" placeholder="johndoe@ymail.com" onChange={insertEmail} />
+                </div>
             </div>
 
             <div className="form-group col-md-4">
-                <label htmlFor="inputCity">City</label>
-                <input type="text" className="form-control" id="inputCity" />
+            <label htmlFor="inputID">Customer ID</label>
+                <div>
+                <input type="number" className="form-control" id="inputAddress" placeholder="1" onChange={insertCustID} />
+                </div>
+                <label htmlFor="inputConfirmID">Confirm Customer ID</label>
+                <div>
+                <input type="number" className="form-control" id="inputEmail" placeholder="1" onChange={insertConfirmID}/>
+                </div>
             </div>
-            <div className="form-group col-md-4">
-                <label htmlFor="inputState">State</label>
-                <select id="inputState" className="form-control">
-                    <option selected>Choose...</option>
-                    <option value="AL">Alabama</option>
-                    <option value="AK">Alaska</option>
-                    <option value="AZ">Arizona</option>
-                    <option value="AR">Arkansas</option>
-                    <option value="CA">California</option>
-                    <option value="CO">Colorado</option>
-                    <option value="CT">Connecticut</option>
-                    <option value="DE">Delaware</option>
-                    <option value="DC">District Of Columbia</option>
-                    <option value="FL">Florida</option>
-                    <option value="GA">Georgia</option>
-                    <option value="HI">Hawaii</option>
-                    <option value="ID">Idaho</option>
-                    <option value="IL">Illinois</option>
-                    <option value="IN">Indiana</option>
-                    <option value="IA">Iowa</option>
-                    <option value="KS">Kansas</option>
-                    <option value="KY">Kentucky</option>
-                    <option value="LA">Louisiana</option>
-                    <option value="ME">Maine</option>
-                    <option value="MD">Maryland</option>
-                    <option value="MA">Massachusetts</option>
-                    <option value="MI">Michigan</option>
-                    <option value="MN">Minnesota</option>
-                    <option value="MS">Mississippi</option>
-                    <option value="MO">Missouri</option>
-                    <option value="MT">Montana</option>
-                    <option value="NE">Nebraska</option>
-                    <option value="NV">Nevada</option>
-                    <option value="NH">New Hampshire</option>
-                    <option value="NJ">New Jersey</option>
-                    <option value="NM">New Mexico</option>
-                    <option value="NY">New York</option>
-                    <option value="NC">North Carolina</option>
-                    <option value="ND">North Dakota</option>
-                    <option value="OH">Ohio</option>
-                    <option value="OK">Oklahoma</option>
-                    <option value="OR">Oregon</option>
-                    <option value="PA">Pennsylvania</option>
-                    <option value="RI">Rhode Island</option>
-                    <option value="SC">South Carolina</option>
-                    <option value="SD">South Dakota</option>
-                    <option value="TN">Tennessee</option>
-                    <option value="TX">Texas</option>
-                    <option value="UT">Utah</option>
-                    <option value="VT">Vermont</option>
-                    <option value="VA">Virginia</option>
-                    <option value="WA">Washington</option>
-                    <option value="WV">West Virginia</option>
-                    <option value="WI">Wisconsin</option>
-                    <option value="WY">Wyoming</option>
-                </select>
-            </div>
-            <div className="form-group col-md-4">
-                <label htmlFor="inputZip">Zip</label>
-                <input type="text" className="form-control" id="inputZip" />
-            </div>
-
-
-            <div className="form-group col-md-4">
-                <label htmlFor="inputCardNumber">Card Number</label>
-                <input type="text" className="form-control" id="inputCardNumber" aria-describedby="emailHelp" placeholder="" />
-            </div>
-            <div className="form-group col-md-4">
-                <label htmlFor="inputPIN">PIN</label>
-                <input type="password" className="form-control" id="inputPIN" placeholder="" />
-            </div>
+            <button type="submit" className="btn btn-primary">Create Account</button> 
             <br></br>
-
-            <div className="form-group">
-                <div className="form-check">
-                    <input className="form-check-input" type="checkbox" id="gridCheck" />
-                    <label className="form-check-label" htmlFor="gridCheck">
-                        Checking
-                    </label>
-                </div>
-                <div className="form-check">
-                    <input className="form-check-input" type="checkbox" id="gridCheck" />
-                    <label className="form-check-label" htmlFor="gridCheck">
-                        Savings
-                    </label>
-                </div>
-                <div className="form-check">
-                    <input className="form-check-input" type="checkbox" id="gridCheck" />
-                    <label className="form-check-label" htmlFor="gridCheck">
-                        Money
-                    </label>
-                </div>
-            </div>
             <br></br>
-            <button type="submit" className="btn btn-primary">Create Account</button>
         </form>
     )
 }
