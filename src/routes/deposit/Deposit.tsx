0b2fbc67eac4a6887f7edb336477a7c995582a71
RocknRule <coldstar.band@gmail.com>
2022-06-22 02:10:07 -0400

    adding transfer

--- src/routes/deposit/Deposit.tsx
+++ src/routes/deposit/Deposit.tsx
@@ -4,7 +4,7 @@ import './Deposit.css'
 function Deposit() {
   return (
     <div>
-      <div className="input-group mb-3">
+      <div className="deposit-container">
         <span className="input-group-text">$</span>
         <span className="input-group-text">0.00</span>
         <input type="text" className="form-control" aria-label="Dollar amount (with dot and two decimal places)"/>
--- src/routes/transfer/Transfer.tsx
+++ src/routes/transfer/Transfer.tsx
@@ -30,8 +30,9 @@ function Withdraw(){
             <input className="form-check-input" type="radio" name="inlineRadioOptions1" id="inlineRadio3" value="option3"/>
             <label className="form-check-label" htmlFor="inlineRadio3">Money Market</label>
         </div>
+        <h5>Please choose the amount you want to transfer:</h5>
         <div className="transfer-container">
-            <label htmlFor="inputAddress">Please choosee the amount you want to transfer:</label>
+            <label htmlFor="inputAddress"></label>
             <input type="text" className="form-control" id="inputAddress" placeholder="$$$" /*onChange={}*/ />
         </div>
         <button type="submit" className="btn btn-primary">Submit</button>
