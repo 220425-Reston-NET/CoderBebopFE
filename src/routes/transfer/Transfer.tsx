c73a5653bd45ed2fcab798be98f4e8b192104de6
RocknRule <coldstar.band@gmail.com>
2022-06-21 23:02:54 -0400

    adding transfer

--- src/routes/transfer/Transfer.tsx
+++ src/routes/transfer/Transfer.tsx
@@ -4,22 +4,23 @@ import './Transfer.css'
 function Withdraw(){
     return (
     <>
-    <div className="form-check form-check-inline">
-        <input className="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1"/>
-        <label className="form-check-label" htmlFor="inlineCheckbox1">Checking</label>
-    </div>
-    <div className="form-check form-check-inline">
-        <input className="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2"/>
-        <label className="form-check-label" htmlFor="inlineCheckbox2">Savings</label>
-    </div>
-    <div className="form-check form-check-inline">
-        <input className="form-check-input" type="checkbox" id="inlineCheckbox3" value="option3" />
-        <label className="form-check-label" htmlFor="inlineCheckbox3">Money Market</label>
-    </div>
-
-    <button type="button" className="btn btn-primary btn-lg">Back</button>
-    <button type="button" className="btn btn-primary btn-lg">Continue</button>
+        <div className="form-check form-check-inline">
+            <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"/>
+            <label className="form-check-label" htmlFor="inlineRadio1">Checking</label>
+        </div>
+        <div className="form-check form-check-inline">
+            <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"/>
+            <label className="form-check-label" htmlFor="inlineRadio2">Savings</label>
+        </div>
+        <div className="form-check form-check-inline">
+            <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3"/>
+            <label className="form-check-label" htmlFor="inlineRadio3">Money Market</label>
+        </div>
+        <button type="button" className="btn btn-primary btn-lg">Back</button>
+        <button type="button" className="btn btn-primary btn-lg">Continue</button>
     </>
+
+
     )
 }
 
