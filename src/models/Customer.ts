79702c22dc6a46daf3f5c0918ae5ffe4e27ff8d1
RobertNevarez <lilgosu999@gmail.com>
2022-06-24 01:42:30 -0400

    rough draft of create customer

---
+++ src/models/Customer.ts
@@ -0,0 +1,14 @@
+export interface Customer{
+    custID : number;
+    name : string;
+    phone : string;
+    address : string;
+    email : string;
+    cardNumber : number;
+    pin : number;
+    pinID : number;
+    cAccID : number;
+    sAccID : number;
+    mAccID : number;
+
+}
--- src/routes/newcustinfo/NewCustInfo.tsx
+++ src/routes/newcustinfo/NewCustInfo.tsx
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import './NewCustInfo.css'
-import CreateCustomer from '../createcustomer/CreateCustomer';
+import  CreateCustomer  from '../createcustomer/CreateCustomer';
+import {Customer} from '../../models/Customer'
 
 function NewCustInfo() {
+
+  
+  const [customer, setcustomer] = useState({} as Customer);
+
 
   const navigate = useNavigate();
   const goToWelcome = () => {
@@ -16,8 +21,13 @@ function NewCustInfo() {
     <div className='info-container'>
       <ul className="list-group styling-container">
         <h4>Card Number</h4>
+
+
+        <div>placeholder = {customer.pin}</div>
+
         <li className="list-group-item styling-container"></li>
         <h4>PIN</h4>
+        <div>placeholder={}</div>
         <li className="list-group-item styling-container">random</li>
       </ul>
         <button type="submit" className="btn btn-primary styling-container"><div onClick={goToWelcome}>Back to Login</div></button>
